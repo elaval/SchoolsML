be4281d4014cfb36e054094f7d0ec2b4c5d8a8bd
Ernesto Laval <ernesto.laval@gmail.com>
2019-05-13 22:22:05 -0400

    Check for user on logout

--- src/app/components/common/topnavbar/topnavbar.component.ts
+++ src/app/components/common/topnavbar/topnavbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { smoothlyMenu } from '../../../app.helpers';
 import { DataService } from 'src/app/services/data.service';
 import { AuthService } from 'src/app/services/auth.service';
@@ -9,13 +9,20 @@ declare var jQuery: any;
     styleUrls: ['./topnavbar.css'],
     templateUrl: 'topnavbar.template.html'
 })
-export class TopnavbarComponent {
+export class TopnavbarComponent implements OnInit {
     searchText: string;
+    user: any;
 
     constructor(
         private dataService:DataService,
         private authService:AuthService
     ) {}
+
+    ngOnInit() {
+        this.authService.user.subscribe(user => {
+            this.user = user;
+        })
+    }
 
     toggleNavigation(): void {
         jQuery('body').toggleClass('mini-navbar');
